725ccfdd087ad8bf1ae212275e019fed61f7b0d0
aliyu <aliyusuf661>
2020-04-27 11:15:01 +0300

    xtra updates

--- lib/screens/junkYard.dart
+++ lib/screens/junkYard.dart
@@ -16,6 +16,30 @@ class _JunkYardState extends State<JunkYard> {
       appBar: AppBar(
         title: Text('الملف الشخصي'),
       ),
+      body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+//            mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new Container(
+                    width: 80.0,
+                    height: 80.0,
+                    decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            fit: BoxFit.fill,
+                            image: new NetworkImage(
+                                "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.8S4L0ieIehMsjfO52FNZpgHaHa%26pid%3DApi&f=1")
+                        )
+                    )),
+                new Text("ابو خليل",
+                    textScaleFactor: 1.5)
+              ],
+            ),
+          ),
+      ),
     );
   }
 }
